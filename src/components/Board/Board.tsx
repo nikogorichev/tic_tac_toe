d061742fa421fba60f15e7fc2e9358d6b97bd51c
Николай Горичев <n.gorichev@itssupport.ru>
2024-03-12 21:36:17 +0300

    add computer first move  in board

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,21 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import Button from "shared/Button/Button";
 import styles from "./Board.module.scss";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Cell from "shared/Cell/Cell";
 import { Mark } from "utils/types/Mark";
 import { Winner } from "utils/types/Winner";
+import GameContext from "providers/GameProvider/GameContext";
+
+const lines = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 const calculateWinner = (cellValue: Mark[]): Mark => {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (
@@ -29,18 +32,26 @@ const calculateWinner = (cellValue: Mark[]): Mark => {
   return null;
 };
 
+// const nextComputerStep = (computerMark: Mark, cellsValue: Mark[]) => {
+//   const playerMark: Mark =  computerMark === "x" ? "o" : "x"
+//   lines.forEach((line) => {
+//     // eslint-disable-next-line no-console
+//     console.log(line)
+//     if (line.)
+//   })
+// }
+
 const Board = () => {
+  const { options } = useContext(GameContext);
   const [cells, setCells] = useState<Mark[]>(Array(9).fill(null));
   const [currentPlayer, setCurrentPlayer] = useState<Mark>("x");
   const [winner, setWinner] = useState<Winner>(null);
 
   const setCellValue = (index: number) => {
-    return () => {
-      setCells((prev) =>
-        prev.map((cell, i) => (index === i ? currentPlayer : cell))
-      );
-      setCurrentPlayer((prev) => (prev === "x" ? "o" : "x"));
-    };
+    setCells((prev) =>
+      prev.map((cell, i) => (index === i ? currentPlayer : cell))
+    );
+    setCurrentPlayer((prev) => (prev === "x" ? "o" : "x"));
   };
 
   const reset = () => {
@@ -59,21 +70,48 @@ const Board = () => {
       setWinner("draw");
     }
   }, [cells]);
+
+  useEffect(() => {
+    if (options.game === "cpu") {
+      const computerMark: Mark = options.mark === "x" ? "o" : "x";
+      if (computerMark === currentPlayer) {
+        if (computerMark === "x" && !cells.filter(Boolean).length) {
+          const firstStepIndex = Math.floor(Math.random() * 9);
+          setCellValue(firstStepIndex);
+        }
+        // nextComputerStep(computerMark, cells)
+      }
+      
+    }
+  }, [currentPlayer]);
+
   return (
     <>
+      <div className={styles.result}>
+        {" "}
+        {/* МОЖНО ЛИ ТАК ПИСАТЬ ТЕРНАРНИКИ ИЛИ ТАКУЮ ЛОГИКУ НУЖНО ВЫНОСИТЬ В ОТДЛЕЛЬНЫЕ ФУНКЦИИ ИЛИ ЛИТЕРАЛЬНЫЙ ОБЪЕКТ */}
+        {winner
+          ? winner === "draw"
+            ? "Ничья"
+            : `Победили ${winner.toUpperCase()}`
+          : ""}
+      </div>
+
       <div className={styles.grid}>
         {cells.map((cell, i) => {
           return (
             <Cell
               winner={winner}
               key={i}
-              setCellValue={setCellValue(i)}
+              setCellValue={() => setCellValue(i)}
               value={cell}
             />
           );
         })}
       </div>
-      <Button onClick={reset}>Сбросить</Button>
+      <Button onClick={reset} className={styles.btn}>
+        Сбросить
+      </Button>
     </>
   );
 };
